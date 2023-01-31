32ed159aaff84bc014ba34cd67d7e067ab887324
Guilherme Alves <guialvescarvalho.gac@gmail.com>
2023-01-31 16:16:56 -0300

    final

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,9 +22,44 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+        */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devCamila.getConteudosInscritos());
+        System.out.println("- Progredir");
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("Conteúdos inscritos: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularXp());
+
+        System.out.println("------------------------");
+
+
+        Dev devGuilherme = new Dev();
+        devGuilherme.setNome("Guilherme");
+        devGuilherme.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devGuilherme.getConteudosInscritos());
+        System.out.println("- Progredir");
+        devGuilherme.progredir();
+        devGuilherme.progredir();
+        devGuilherme.progredir();
+        System.out.println("Conteúdos inscritos: " + devGuilherme.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos: " + devGuilherme.getConteudosConcluidos());
+        System.out.println("XP: " + devGuilherme.calcularXp());
 
     }
 }
